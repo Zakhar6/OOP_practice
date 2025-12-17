95d72444189250ae5bc2949a0d8390fefa6375eb
Zahar <genakrokodil587@gmail.com>
2025-12-17 19:11:43 +0100

    done practical work №5

--- lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2.cpp
@@ -1,19 +1,53 @@
 ﻿#include <iostream>
+#include <string>
+#include "Person.h"
+#include "Teacher.h"
+#include "Seller.h"
+#include "Doctor.h"
 #include "Student.h"
 
+int chooseClass() {
+    int choice;
+    std::cout << "Виберіть клас для створення об'єкта:\n";
+    std::cout << "1. Teacher\n";
+    std::cout << "2. Seller\n";
+    std::cout << "3. Doctor\n";
+    std::cout << "4. Student\n";
+    std::cout << "Введіть номер: ";
+    std::cin >> choice;
+    return choice;
+}
+
 int main() {
-    Student s1, s2;
+    Person* arr[5];
 
-    std::cin >> s1;
-    std::cin >> s2;
+    for (int i = 0; i < 5; ++i) {
+        int choice = chooseClass();
+        switch (choice) {
+        case 1:
+            arr[i] = new Teacher();
+            break;
+        case 2:
+            arr[i] = new Seller();
+            break;
+        case 3:
+            arr[i] = new Doctor();
+            break;
+        case 4:
+            arr[i] = new Student();
+            break;
+        default:
+            std::cout << "Невірний вибір, об'єкт Student створено за замовчуванням.\n";
+            arr[i] = new Student();
+            break;
+        }
+    }
 
-    std::cout << s1 << std::endl;
-    std::cout << s2 << std::endl;
+    for (int i = 0; i < 5; ++i) {
+        arr[i]->print();
+    }
 
-    if (s1 == s2)
-        std::cout << "Students are equal" << std::endl;
-    else
-        std::cout << "Students are not equal" << std::endl;
-
-    return 0;
+    for (int i = 0; i < 5; ++i) {
+        arr[i]->virtualMethod();
+    }
 }
